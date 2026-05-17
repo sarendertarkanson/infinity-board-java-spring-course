ec1055812c920c18f9e0a0a10b82162141e12f4d
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-17 04:23:12 +0200

    loop controls

---
+++ 01-first-java-project/src/main/java/org/example/LoopControlStatements.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class LoopControlStatements {
+
+    public static void main(String[] args) {
+
+        int[] numbers= {10,20,30,40,50};
+        int target = 30;
+
+        for (int i=0; i<numbers.length; i++) {
+            if (numbers[i] == target) {
+                System.out.println("Target found at index: " + i);
+                break;
+            }
+        }
+
+        for (int i=1; i<=10; i++){
+            if(i%2 ==0){
+                continue;
+            }
+            System.out.println("Odd number: " + i);
+        }
+
+    }
+
+}
