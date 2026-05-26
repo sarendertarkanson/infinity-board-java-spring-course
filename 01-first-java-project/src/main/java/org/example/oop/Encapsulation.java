697d0293a417eaa79cb0db7df1fd25498d7fd464
Alparslan Tarkan <atarkan@hotmail.com>
2026-05-26 18:40:16 +0200

    reusing code with inheritance

--- 01-first-java-project/src/main/java/org/example/oop/Encapsulation.java
+++ 01-first-java-project/src/main/java/org/example/oop/Encapsulation.java
@@ -1,41 +1,41 @@
-package org.example.oop;
-
-class BankAccount {
-    private String accountHolder;
-    private double balance;
-
-    public BankAccount() {
-        this.accountHolder = "John";
-        this.balance = 1000;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        if (balance >= 0) {
-            this.balance = balance;
-        } else {
-            System.out.println("Invalid balance amount");;
-        }
-    }
-}
-
-public class Encapsulation {
-    public static void main(String[] args) {
-
-        BankAccount bankAccount = new BankAccount();
-
-        System.out.println("Balance is " + bankAccount.getBalance());
-
-        bankAccount.setBalance(2000);
-
-        System.out.println("Balance is " + bankAccount.getBalance());
-
-        bankAccount.setBalance(-1000);
-
-
-    }
-
-}
+//package org.example.oop;
+//
+//class BankAccount {
+//    private String accountHolder;
+//    private double balance;
+//
+//    public BankAccount() {
+//        this.accountHolder = "John";
+//        this.balance = 1000;
+//    }
+//
+//    public double getBalance() {
+//        return balance;
+//    }
+//
+//    public void setBalance(double balance) {
+//        if (balance >= 0) {
+//            this.balance = balance;
+//        } else {
+//            System.out.println("Invalid balance amount");;
+//        }
+//    }
+//}
+//
+//public class Encapsulation {
+//    public static void main(String[] args) {
+//
+//        BankAccount bankAccount = new BankAccount();
+//
+//        System.out.println("Balance is " + bankAccount.getBalance());
+//
+//        bankAccount.setBalance(2000);
+//
+//        System.out.println("Balance is " + bankAccount.getBalance());
+//
+//        bankAccount.setBalance(-1000);
+//
+//
+//    }
+//
+//}
