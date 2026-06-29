735c84432a78233bd525e7897b7a0b6364ed5034
Alparslan Tarkan <atarkan@hotmail.com>
2026-06-29 17:12:03 +0200

    different ways to inject dependencies

--- 02-MySpringApp/src/main/java/com/example/core/OrderService.java
+++ 02-MySpringApp/src/main/java/com/example/core/OrderService.java
@@ -9,6 +9,17 @@ public class OrderService {
     @Autowired
     private PaymentService paymentService;
 
+//    public OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
+
+//    @Autowired
+//    public void setPaymentService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
+
+
+
     public void placeOrder() {
         paymentService.processPayment();
         System.out.println("Order placed successfully.");
