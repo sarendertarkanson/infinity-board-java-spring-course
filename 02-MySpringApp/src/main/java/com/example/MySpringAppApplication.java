54a9fccf6f4c478da6b0a5798a8f3abb1ff8dd92
Alparslan Tarkan <atarkan@hotmail.com>
2026-06-30 17:02:20 +0200

    what is a spring bean

--- 02-MySpringApp/src/main/java/com/example/MySpringAppApplication.java
+++ 02-MySpringApp/src/main/java/com/example/MySpringAppApplication.java
@@ -2,13 +2,12 @@ package com.example;
 
 import com.example.config.AppConfig;
 import com.example.core.OrderService;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MySpringAppApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         OrderService orderService = context.getBean(OrderService.class);
         orderService.placeOrder();
     }
